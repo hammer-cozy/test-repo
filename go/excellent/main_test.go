27d9498e7ba5716e3748fa5d166007b281617d68
hammer-cozy <hammer-cozy@gmail.com>
2025-01-27 22:26:42 +0900

    add test.yml

---
+++ go/excellent/main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestEvenOrOdd(t *testing.T) {
+	tests := []struct {
+		name string
+		n    int
+		want string
+	}{
+		{"Even", 2, "Even"},
+		{"Odd", 3, "Odd"},
+		{"Even", 4, "Even"},
+		{"Odd", 5, "Odd"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := EvenOrOdd(tt.n); got != tt.want {
+				t.Errorf("EvenOrOdd() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
